475aafd9fbf4ca54da9b31b2a4169942e4bf222f
None <None>
2021-05-24 18:27:51 -0700

    Fix script

--- build_pcc.sh
+++ build_pcc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone https://github.com/PCCproject/PCC-Kernel.git /tmp
+git clone https://github.com/PCCproject/PCC-Kernel.git /tmp/PCC-Kernel
 cd /tmp/PCC-Kernel/src
 make
 sudo insmod tcp_pcc.ko
+rm -rf /tmp/PCC-Kernel
